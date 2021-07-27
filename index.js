6c7bd03db80d6911d1fd25d0066909fc9df95df5
baikseungjeon <tmdwjsofficial@naver.com>
2021-07-28 01:31:06 +0900

    nodejs 1

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const port = 5000
 const bodyParser = require('body-parser');
 const {User} = require('./models/User');
 
+const config = require('./config/key');
+
 // application/x-www-form-urlencoded 분석해서 가지고 오게끔
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json을 분석해서 가지고 오게끔 
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://tmdwjs:qwert12345@cluster0.wcno7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     userNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
     
-app.get('/', (req, res) => res.send('Hello World! test1111'))
+app.get('/', (req, res) => res.send('Hello World! test1111 테스트 중입니다'))
 
 app.post('/register', (req, res) => {
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어줌
