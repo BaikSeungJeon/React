bbf31561ee49445b713396bf04c31e1d3fda39c9
baikseungjeon <tmdwjsofficial@naver.com>
2021-07-28 16:19:40 +0900

    nodejs 1

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const {User} = require('./models/User');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const {User} = require('./models/User');
 
 // application/x-www-form-urlencoded 분석해서 가지고 오게끔
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json을 분석해서 가지고 오게끔 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -28,6 +29,37 @@ app.post('/register', (req, res) => {
     if (err) return res.json({success : false, err})
     return res.status(200).json({
       success : true
+    })
+  })
+})
+
+// 210728
+app.post('/login', (req, res) => {
+
+
+  // 1. 데이터베이스 안에서 요청된 이메일 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "가입 안 된 사용자로 확인됩니다."
+      })
+    }
+  // 2. 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인하기
+
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if(!isMatch) // isMatch가 없다면 비밀번호가 같지 않다는 거니까
+    return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+  // 3. 비밀번호까지 맞다면 토큰 생성
+    user.generateToken((err, user) => {
+      if(err) return res.status(400).send(err);
+      
+      // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지
+      res.cookie("x_auth", user.token)
+      .status(200) // 200 성공 400 실패
+      .json({loginSuccess: true, userId: user._id})
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10 
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,53 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+// 210728
+// 비밀번호를 암호화 시키는 작업
+userSchema.pre('save', function(next){
+    
+    var user = this;
+
+    // model 안에 password가 변할 때만 bcrypt를 이용해서 암호화 해준다.
+    if(user.isModified('password')) {
+
+        bcrypt.genSalt(saltRounds, function(err, salt){ // salt를 만들 때 saltRounds가 필요
+            if (err) return next(err)
+    
+            bcrypt.hash( user.password , salt , function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        }) 
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword 1234567 암호화 된 비밀번호랑 같다
+
+    bcrypt.compare(plainPassword, this.password, function(err, inMatch) {
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+    // jsonwebtoken을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    // user._id + 'secretToken' = token
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema) // 모델로 스키마를 감싸는 과정
 
