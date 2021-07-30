d3221f70b8635d6b3cc31271efc895b080a99512
baikseungjeon <tmdwjsofficial@naver.com>
2021-07-30 17:01:37 +0900

    react

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>
-            Login Page
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form>
+                <label>Email</label>
+                <input type="email" value onChange />
+                <label>Password</label>
+                <input type="Password" value onChange />
+                <br/>
+                <button>login</button>
+            </form>
         </div>
     )
 }
