d46fa71538a22c236e95ecda7a450f2b7f9be5d5
baikseungjeon <tmdwjsofficial@naver.com>
2021-08-03 22:11:19 +0900

    react2

--- src/App.js
+++ src/App.js
@@ -7,17 +7,12 @@ import './App.css';
 function App() {
 
   // var [a, b] = [10, 100]; a = 10, b = 100
-
   let [글제목, 글제목변경] = useState(['강남 맛집', '홍대 맛집', '이태원 맛집']);
   // [사용 프레임워크 , state 정정해주는 함수]
   let [따봉, 업] = useState(0);
-  // let [modal, modal변경] = useState(false);
   let [modal, modal열기] = useState(false);
-
-
-
-
-  // let post = '첫 포스팅';
+  let [누른제목, 누른제목변경] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
 
   // state 복사본 만들어서 배열 [0] 수정하기
@@ -27,6 +22,7 @@ function App() {
     newArray[0] = '강남 숨은 맛집';
     글제목변경( newArray ); // state를 아에 대체해주는 함수
   }
+
 
 // 버튼을 누르면 제목들을 오름차순으로 정렬하기
   function 정렬함수(){
@@ -48,32 +44,42 @@ function App() {
 
       <button onClick={ 변경함수 }> 변경 </button>
       <button onClick={ 정렬함수 }> 정렬 </button>
-      <div className="list">
-        <h3> { 글제목[0] } <span onClick={ () => { 업(따봉 + 1) } }>👍</span> { 따봉 } </h3>
-        <p> 2021년 8월 2일 작성 </p>
-        <hr/>
+
+      {
+        글제목.map(function(a, i){
+          return (
+          <div className="list" key={i}>
+            <h3 onClick={ () => { 누른제목변경(i) } }>
+              { a } <span onClick={ () => { 업(따봉 + 1) } }>👍</span> { 따봉 }
+            </h3>
+            <p> 2021년 8월 2일 작성 </p>
+            <hr/>
+          </div>
+          )
+        })
+      }
+
+      {/* <button onClick={ () => { 누른제목변경(0) } }> 버튼 1 </button>
+      <button onClick={ () => { 누른제목변경(1) } }> 버튼 2 </button>
+      <button onClick={ () => { 누른제목변경(2) } }> 버튼 3 </button> */}
+      
+      
+      <div className="input">
+        <input onChange = { (e) => { 입력값변경(e.target.value) } } />
+        <button onClick = { () => {
+
+          let 글제목사본 = [...글제목];
+          글제목사본.unshift(입력값);
+          글제목변경(글제목사본)
+        } }> 입력 </button>
       </div>
-      <div className="list">
-        <h3> { 글제목[1] } </h3>
-        <p> 2021년 8월 3일 작성 </p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3> { 글제목[2] } </h3>
-        <p> 2021년 8월 4일 작성 </p>
-        <hr/>
-      </div>
+
       <button onClick={ () => { modal열기(!modal) } }> modal </button>
 
-      {/* {
-        modal === true
-        ? <Modal/>
-        : null
-      } */}
-      
+
       {
         modal === true
-        ? <Modal/>
+        ? <Modal 글제목={글제목} 누른제목={누른제목}> </Modal>
         : null
       }
 
@@ -84,10 +90,10 @@ function App() {
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-        <h2>제목</h2>
+        <h2>제목 { props.글제목[props.누른제목] }</h2>
         <p>날짜</p>
         <p>내용</p>
 
