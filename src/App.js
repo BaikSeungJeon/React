8f3f74cb7be3a856f14f78cd93f0692b3482fc9b
baikseungjeon <tmdwjsofficial@naver.com>
2021-08-06 17:38:41 +0900

    shop

--- src/App.js
+++ src/App.js
@@ -1,78 +1,84 @@
 /* eslint-disable */
+
 import React, {useState} from 'react';
 import { Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
-
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail.js';
 function App() {
 
   let [shoes, shoesChange] = useState(Data);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Muscle Shop</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Container>
+          <Navbar.Brand href="#home">Muscle Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link><Link to="/"> Home </Link></Nav.Link>
+              <Nav.Link><Link to="/detail"> Detail </Link></Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-<div className="jumbotron">
-    <h1>런닝화 50% SALE!</h1>
-    <p>
-      This is a simple hero unit, a simple jumbotron-style component for calling
-      extra attention to geatured content or information.
-    </p>
-    <p>
-      <Button variant="primary">Learn more</Button>
-    </p>
-    
-</div>
+      <Route exact path="/">
+        <div>
+          <div className="jumbotron">
+            <h1>런닝화 50% SALE!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to geatured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+            <div className="container">
+              <div className="row">
+                { // { } 안에는 map 함수
+                  shoes.map( (a, i) => { // state.map(()=>{return})
+                    // (a, i) a shoes 안에 하나하나 데이터, i는 반복문 돌 때마다의 정수
+                    return (
+                      <Card shoes={shoes[i]} i={i} />
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </Route>
 
-<div className="container">
-  <div className="row">
-    <Col></Col>
-    <Col></Col>
-    <Col></Col>
-  </div>
-</div>
+      <Route path="/detail">
+        <Detail  />
+      </Route>
+
+      <Route path="/:id">
+      </Route>
     </div>
   );
 }
 
-function Col(){
+
+// component 하나 더 추가해서 
+function Card(props){ // 상위 컴포넌트에 있는 걸 가지고 와야 하니, props로 가지고 오기
   return (
-    <>
       <div className="col-md-4">
-        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-        <h4>{shoes[0].title}</h4>
-        <p>{shoes[0].content}</p>
+        {/* img에 데이터 바인딩 하려면 src={}로 묶어주기 */}
+        <img src={ "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"} width="100%" />
+        <h4>{props.shoes.title}</h4>
+        <p>{props.shoes.content} & {props.shoes.price} </p>
       </div>
-
-      <div className="col-md-4">
-        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-        <h4>{shoes[1].title}</h4>
-        <p>{shoes[1].content}</p>
-      </div>
-
-      <div className="col-md-4">
-        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-        <h4>{shoes[2].title}</h4>
-        <p>{shoes[2].content}</p>
-      </div>
-    </>
   )
 }
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 /* eslint-disable*/
+
 let Data = [
     {
       id : 0,
