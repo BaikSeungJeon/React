153db0446c027b9715970c35beded6d7e673a57b
baikseungjeon <tmdwjsofficial@naver.com>
2021-08-09 23:31:54 +0900

    shop

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
       </Route>
 
       <Route path="/detail">
-        <Detail  />
+        <Detail shoes={shoes} />
       </Route>
 
       <Route path="/:id">
--- src/Detail.js
+++ src/Detail.js
@@ -3,7 +3,7 @@
 import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
 
-function Detail() {
+function Detail(props) {
 
     let history = useHistory();
 
@@ -15,9 +15,9 @@ function Detail() {
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             </div>
             <div className="col-md-6 mt-4">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
+                <h4 className="pt-5">{props.shoes[0].title}</h4>
+                <p>{props.shoes[0].content}</p>
+                <p>{props.shoes[0].price}</p>
                 <button className="btn btn-danger">주문하기</button> 
                 <button className="btn btn-danger" onClick={()=>{
                     history.goBack();
