a348fbd18e3964c5036a054731d8bde1347081a9
baikseungjeon <tmdwjsofficial@naver.com>
2021-08-10 18:26:07 +0900

    shop

--- src/Detail.js
+++ src/Detail.js
@@ -1,23 +1,43 @@
 /* eslint-disable*/
 
 import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let box = styled.div`
+    padding : 20px;
+`;
+let box2 = styled.h4`
+    font-size: 25px;
+    color : ${props => props.색상}
+`;
 
 function Detail(props) {
 
+    let { id } = useParams();
     let history = useHistory();
 
     return (
         <div>
         <div className="container">
+
+            <box>
+                <box2 className="red"> Detail </box2>
+            </box>
+
+            <div className="my-alert2">
+                <p> 재고가 얼마 남지 않았습니다. </p>
+            </div>
+
             <div className="row">
                 <div className="col-md-6">
                 <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
             </div>
             <div className="col-md-6 mt-4">
-                <h4 className="pt-5">{props.shoes[0].title}</h4>
-                <p>{props.shoes[0].content}</p>
-                <p>{props.shoes[0].price}</p>
+                <h4 className="pt-5">{props.shoes[id].title}</h4>
+                <p>{props.shoes[id].content}</p>
+                <p>{props.shoes[id].price}</p>
                 <button className="btn btn-danger">주문하기</button> 
                 <button className="btn btn-danger" onClick={()=>{
                     history.goBack();
