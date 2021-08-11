de843db918a5e5d2936cb8d518004a20f73468f8
baikseungjeon <tmdwjsofficial@naver.com>
2021-08-11 18:22:16 +0900

    shop

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,6 @@
 /* eslint-disable*/
 
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import './Detail.scss';
@@ -15,6 +15,24 @@ let box2 = styled.h4`
 
 function Detail(props) {
 
+
+    let [alert, alertChange] = useState(true);
+    let [inputData, inputDataChange] = useState('');
+
+
+    useEffect(() => {
+        // 2초 후에 alert 창 안 보이게 해주셈
+        let timer = setTimeout(() => {alertChange(false)}, 2000)
+        return () => { clearTimeout(timer)}
+        // 여러가지 이유로 버그 생성을 막기 위해, setTimeout을 끝내는 스킬
+        
+    }, []);
+    // 밑에 alert를 넣어줌으로써 alert란 state가 변경될 때마다 작동
+    // 당연히 alert state를 위에다가 작성해 주어야 함.
+
+
+
+
     let { id } = useParams();
     let history = useHistory();
 
@@ -26,9 +44,26 @@ function Detail(props) {
                 <box2 className="red"> Detail </box2>
             </box>
 
-            <div className="my-alert2">
-                <p> 재고가 얼마 남지 않았습니다. </p>
-            </div>
+
+
+
+
+            <input onChange={(e)=>{inputDataChange(e.target.value)}} />
+
+
+
+
+
+
+            {/* UI를 항상 켜 놓는 게 아니라면, 이런 식으로 스위치를 만든다 */}
+            {
+                alert === true
+                ? <div className="my-alert2">
+                    <p> 재고가 얼마 남지 않았습니다. </p>
+                </div>
+                : null
+            }
+            
 
             <div className="row">
                 <div className="col-md-6">
