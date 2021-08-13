96775cb653267fedfe4e0ba4b7147d7270330a09
baikseungjeon <tmdwjsofficial@naver.com>
2021-08-14 01:32:49 +0900

    shop

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Data from './data.js';
 import { Link, Route, Switch } from 'react-router-dom';
 import Detail from './Detail.js';
 import axios from 'axios';
+import Cart from './Cart.js';
+
+
 
 function App() {
 
@@ -35,58 +38,66 @@ function App() {
         </Container>
       </Navbar>
 
-      <Route exact path="/">
-        <div>
-          <div className="jumbotron">
-            <h1>런닝화 50% SALE!</h1>
-            <p>
-              This is a simple hero unit, a simple jumbotron-style component for calling
-              extra attention to geatured content or information.
-            </p>
-            <p>
-              <Button variant="primary">Learn more</Button>
-            </p>
-            <div className="container">
-              <div className="row">
-                { // { } 안에는 map 함수
-                  shoes.map( (a, i) => { // state.map(()=>{return})
-                    // (a, i) a shoes 안에 하나하나 데이터, i는 반복문 돌 때마다의 정수
-                    return (
-                      <Card shoes={shoes[i]} i={i} />
-                    )
+      <Switch>
+        <Route exact path="/">
+          <div>
+            <div className="jumbotron">
+              <h1>런닝화 50% SALE!</h1>
+              <p>
+                This is a simple hero unit, a simple jumbotron-style component for calling
+                extra attention to geatured content or information.
+              </p>
+              <p>
+                <Button variant="primary">Learn more</Button>
+              </p>
+              <div className="container">
+                <div className="row">
+                  { // { } 안에는 map 함수
+                    shoes.map( (a, i) => { // state.map(()=>{return})
+                      // (a, i) a shoes 안에 하나하나 데이터, i는 반복문 돌 때마다의 정수
+                      return (
+                        <Card shoes={shoes[i]} i={i} />
+                      )
+                    })
+                  }
+                </div>
+
+                <button className="btn btn-primary" onClick={() => {
+
+
+                  axios.post('서버URL', { id : 'black', pw : '1234'}).then()
+
+
+                  // 로딩 중이라는 UI 띄움
+
+                  axios.get('https://codingapple1.github.io/shop/data2.json')
+                  .then((result)=>{ // 성공하면 로딩 중이라는 UI 지움
+                    console.log(result.data)
+                    shoesChange([...shoes, ...result.data]);
                   })
-                }
+                  .catch(()=>{ // 실패하면
+                    console.log("실패했어요")
+                  })
+                }}>더보기</button>
+
               </div>
-
-              <button className="btn btn-primary" onClick={() => {
-
-
-                axios.post('서버URL', { id : 'black', pw : '1234'}).then()
-
-
-                // 로딩 중이라는 UI 띄움
-
-                axios.get('https://codingapple1.github.io/shop/data2.json')
-                .then((result)=>{ // 성공하면 로딩 중이라는 UI 지움
-                  console.log(result.data)
-                  shoesChange([...shoes, ...result.data]);
-                })
-                .catch(()=>{ // 실패하면
-                  console.log("실패했어요")
-                })
-              }}>더보기</button>
-
             </div>
           </div>
-        </div>
-      </Route>
+        </Route>
 
-      <Route path="/detail/:id">
-        <Detail shoes={shoes} Info = {Info} InfoChange = {InfoChange} />
-      </Route>
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} Info = {Info} InfoChange = {InfoChange} />
+        </Route>
 
-      <Route path="/:id">
-      </Route>
+        <Route path="/cart"> 
+          <Cart />
+        </Route>
+
+        <Route path="/:id">
+          <div>해결</div>
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
