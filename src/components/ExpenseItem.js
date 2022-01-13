ea3c4aae07efdbafb005bc98fa02d6c380748899
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-01-14 01:21:34 +0900

    0114

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+ function ExpenseItem(){
+     return (
+         <div>
+            <div>2022 01 14 Fri</div>
+            <div>
+                <h3>Car insurance</h3>
+                <div>$100.00</div>
+            </div>
+         </div> 
+     )
+ }
+
+ export default ExpenseItem;
