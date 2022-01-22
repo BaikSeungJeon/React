e9b54d3b321f0403848c897e1b06a5fd66c089f7
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-01-23 00:32:59 +0900

    0123

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+ import './ExpenseItem.css';
+ 
  function ExpenseItem(){
      return (
-         <div>
+         <div className='expense-item'>
             <div>2022 01 14 Fri</div>
-            <div>
-                <h3>Car insurance</h3>
-                <div>$100.00</div>
+            <div className='expense-item__description'>
+                <h2>Car insurance</h2>
+                <div className='expense-item__price'>$100.00</div>
             </div>
          </div> 
      )
