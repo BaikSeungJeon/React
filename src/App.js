9a57c3d21145e9e80e8ed936777c3f3698d8e2e4
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-02-01 19:52:21 +0900

    0201

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import logo from './logo.svg';
 import ExpenseItem  from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car insurance',
+      amount: 5000.00,
+      date: new Date(2022, 2, 1)
+    },
+    {
+      id: 'e2',
+      title: 'TV',
+      amount: 2000.00,
+      date: new Date(2022, 2, 1)
+    },
+    {
+      id: 'e3',
+      title: 'Mac Book',
+      amount: 1000.00,
+      date: new Date(2022, 2, 1)
+    },
+    {
+      id: 'e4',
+      title: 'iPhone Case',
+      amount: 1000.00,
+      date: new Date(2022, 2, 1)
+    }
+  ];
+
   return (
     <div>
       <h1>Study React!</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem 
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem 
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem 
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem 
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
