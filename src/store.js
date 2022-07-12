5fdcb964a5a7938a97668a1f384167ca0840f572
BaikSeungJeon <tmdwjsofficial@naver.com>
2022-07-12 23:42:29 +0900

    redux 활용

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+let cart = createSlice({
+    name: 'state 이름',
+    initialState: 'state 실제 값'
+})
 
 export default configureStore({
     reducer: {
-        
+
     }
 })
